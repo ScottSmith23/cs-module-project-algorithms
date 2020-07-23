9ad83b9034af34c92a52e0545b7954df55cf22f0
Scott Smith <scthsm@outlook.com>
2020-07-22 18:03:30 -0600

    rps done

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,9 +4,24 @@ import sys
 
 def rock_paper_scissors(n):
   # Your code here
+  rounds = n
+  outcomes = []
+  plays = [["rock"], ["paper"], ["scissors"]]
+  if n == 0:
+    return [[]]
+  
+  def play(round,rounds):
+    if rounds == 0:
+      outcomes.append(round)
+      return outcomes
+    for i in range(0,3):
+      play(round + plays[i], rounds - 1)
 
-  pass
 
+  play([],rounds)
+  return outcomes
+
+print(rock_paper_scissors(2))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
